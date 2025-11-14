cdfe0ab020bae0561e280ba925618e7f85f5d648
Dorren Chen <dorrenchen@gmail.com>
2025-11-14 08:14:56 -0500

    vibe coding test

---
+++ tests/parrotpy/functions/test_vibe_coding.py
@@ -0,0 +1,10 @@
+import pytest
+
+def test_prompt(spark):
+    """
+    prompt used:
+
+    populate test_prompt() function body by using available functions from parrotpy.functions, generate new dataframe with a new column called "name" with values are people's name, another column called "age" with values from uniform distribution with min value 0 and max value 80.
+    """
+    pass
+
